89fa4498d9f7dd337c95280f829e2fc27ae876cc
KoalHack <alexandre.saner@gmail.com>
2023-09-13 19:25:15 +0200

    feat(index): load YAML files and interact with

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 #!/usr/bin/env node
+
+//NOTE: Import dependencies
+import fs from 'fs';
+import YAML from 'js-yaml';
+import chalk from 'chalk';
+import { Command } from 'commander';
+
+//NOTE: Load Infos YAML file
+const infosRaw = fs.readFileSync('Data/infos.yml');
+const infos = YAML.load(infosRaw);
+
+//NOTE: Load Logo YAML file
+const logoRaw = fs.readFileSync('Data/logo.yml');
+const logo = YAML.load(logoRaw);
+
+console.log(infos);
